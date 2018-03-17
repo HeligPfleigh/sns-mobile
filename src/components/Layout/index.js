1422645d64189a42a1f31aca16af30bfac057730
Le Linh <linh.le@mttjsc.com>
2018-03-17 13:55:21 +0700

    update layout component

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -1,46 +1,15 @@
 import React, { Component } from "react";
 import { Platform } from "react-native";
-import { Container, Header, Content, Footer, FooterTab } from "native-base";
+import { Container, Header, Content } from "native-base";
 
-import { FTButton } from "../FooterTab";
+import styles from "./styles";
 
 class Layout extends Component {
   render() {
     return (
-      <Container>
+      <Container style={styles.container}>
         {Platform.OS === "ios" && <Header style={{ height: 22 }} />}
         <Content>{this.props.children}</Content>
-        <Footer>
-          <FooterTab>
-            <FTButton name="home" iconStyle={{ fontSize: 26 }} onPress={() => this.props.navigation.navigate("Home")} />
-            <FTButton
-              name="chatbubbles"
-              iconStyle={{ fontSize: 26 }}
-              onPress={() => this.props.navigation.navigate("BlankScreen")}
-            />
-            <FTButton
-              name="contacts"
-              active
-              iconStyle={{ fontSize: 26 }}
-              onPress={() => this.props.navigation.navigate("BlankScreen")}
-            />
-            <FTButton
-              name="notifications"
-              iconStyle={{ fontSize: 26 }}
-              onPress={() => this.props.navigation.navigate("BlankScreen")}
-            />
-            <FTButton
-              name="search"
-              iconStyle={{ fontSize: 26 }}
-              onPress={() => this.props.navigation.navigate("BlankScreen")}
-            />
-            <FTButton
-              name="menu"
-              iconStyle={{ fontSize: 26 }}
-              onPress={() => this.props.navigation.navigate("DrawerOpen")}
-            />
-          </FooterTab>
-        </Footer>
       </Container>
     );
   }
