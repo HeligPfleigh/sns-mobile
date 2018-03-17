e2e86258f4daa1a2467478b947fe0e89bb6a0a4f
Le Linh <linh.le@mttjsc.com>
2018-03-17 13:57:35 +0700

    update friend box screen

--- src/container/FriendsBox/index.js
+++ src/container/FriendsBox/index.js
@@ -1,76 +1,40 @@
-import React, { Component } from "react"
+import React, { Component } from "react";
+import { Text, Tab, Tabs, TabHeading, Icon } from "native-base";
 
-import {
-  Container,
-  Header,
-  Title,
-  View,
-  Content,
-  Text,
-  Button,
-  FooterTab,
-  Left,
-  Right,
-  Body,
-  Tab,
-  Tabs,
-  TabHeading
-} from "native-base"
-
-import Icon from "react-native-vector-icons/FontAwesome"
-import styles from "./styles"
-import Tab1 from "./friendBoxComponents/tab1"
-import Tab3 from "./friendBoxComponents/tab3"
+import Layout from "../../components/Layout";
+import FriendSearch from "./friendBoxComponents/FriendSearch";
+import FriendRequest from "./friendBoxComponents/FriendRequest";
 
 class FriendsBox extends Component {
   render() {
-    const param = this.props.navigation.state.params
     return (
-      <Container style={styles.container}>
-        <Header>
-          <Left>
-            <Button transparent onPress={() => this.props.navigation.goBack()}>
-              <Icon name="angle-left" style={{ fontSize: 27 }} />
-            </Button>
-          </Left>
+      <Layout navigation={this.props.navigation}>
+        <Tabs>
+          <Tab
+            heading={
+              <TabHeading>
+                <Icon name="search" />
+                <Text>Tìm kiếm</Text>
+              </TabHeading>
+            }
+          >
+            <FriendSearch />
+          </Tab>
 
-          <Body style={{ flex: 3 }}>
-            <Title>
-              <Text>Friends box</Text>
-            </Title>
-          </Body>
-
-          <Right />
-        </Header>
-
-        <Content padder>
-          <Tabs>
-            <Tab
-              heading={
-                <TabHeading>
-                  <Icon name="search" />
-                  <Text>Tìm kiếm</Text>
-                </TabHeading>
-              }
-            >
-              <Tab1 />
-            </Tab>
-
-            <Tab
-              heading={
-                <TabHeading>
-                  <Icon name="user-plus" />
-                  <Text>Yêu cầu</Text>
-                </TabHeading>
-              }
-            >
-              <Tab3 />
-            </Tab>
-          </Tabs>
-        </Content>
-      </Container>
-    )
+          <Tab
+            heading={
+              <TabHeading>
+                <Icon name="md-person-add" />
+                <Text>Yêu cầu</Text>
+              </TabHeading>
+            }
+          >
+            <FriendRequest />
+          </Tab>
+        </Tabs>
+      </Layout>
+    );
   }
 }
 
-export default FriendsBox
+export default FriendsBox;
